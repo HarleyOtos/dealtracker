f5cfb29c344bfebde865868d7c84e8789278d640
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-11-02 09:38:52 +0100

    added proper indentation

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,14 +14,14 @@ export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
 
-    if(priceText) {
+    if (priceText) {
       const cleanPrice = priceText.replace(/[^\d.]/g, '');
 
-      let firstPrice; 
+      let firstPrice;
 
       if (cleanPrice) {
         firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
-      } 
+      }
 
       return firstPrice || cleanPrice;
     }
